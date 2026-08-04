aa43ce9a667ae59a610b1cfc317b141373e78a9e
dbbarimah <dbbarimah@gmail.com>
2026-08-04 20:37:14 +0000

    Update data.sql

--- data.sql
+++ data.sql
@@ -1,2 +1,5 @@
 /* The purpose of this file is to enter the queries to insert the data after
-the tables have been created (Phase 5)  */
+the tables have been created (Phase 5) 
+At least 30 entries for the major tables.
+Does not need to be real or existing data.
+*/
