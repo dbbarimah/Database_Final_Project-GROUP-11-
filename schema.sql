28d813e084f9be0c8a7df8e75fd2c6a03b40e13c
dbbarimah <dbbarimah@gmail.com>
2026-08-04 20:38:15 +0000

    Add comments to schema.sql for clarity
    Added comments to clarify the purpose and tasks of the schema.

--- schema.sql
+++ schema.sql
@@ -1,1 +1,5 @@
--- The purpose of this file is to create the tables of the database (Phase 4).
+/* The purpose of this file is to create the tables of the database (Phase 4).
+Enforce constraints
+Add indexes
+Add sequences
+*/
